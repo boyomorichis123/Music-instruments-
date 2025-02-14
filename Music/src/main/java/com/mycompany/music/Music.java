956c28ccd445a78be8afa422e5756df240bbee85
boyomorichis123 <youngdollarrichisboy@gmail.com>
2025-02-14 17:14:59 +0100

    Add files via upload

---
+++ Music/src/main/java/com/mycompany/music/Music.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.music;
+
+/**
+ *
+ * @author HP
+ */
+public class Music {
+
+    public static void main(String[] args) {
+         Loginpage login = new Loginpage();
+        login.setVisible(true);
+        
+        
+       
+    }
+}
